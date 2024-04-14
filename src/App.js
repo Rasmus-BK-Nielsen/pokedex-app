5c92ca68aef350c2dbf1f23a72cdafe9e4c30848
Rasmus B.K. Nielsen <rasmus.bk.nielsen@gmail.com>
2024-04-14 23:36:14 +0200

    pokeAPI_fetchPokemon_complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function PokemonList() {
+     const [pokemon, setPokemon] = useState([]);
+     const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+     const [nextPageUrl, setNextPageUrl] = useState();
+     const [prevPageUrl, setPrevPageUrl] = useState();
+
+     useEffect(() => {
+          async function fetchData() {
+               const response = await fetch(currentPageUrl);
+               const data = await response.json();
+               setPokemon(data.results);
+               setNextPageUrl(data.next);
+               setPrevPageUrl(data.previous);
+          }
+          fetchData();
+     }, [currentPageUrl]);
+
+     function gotoNextPage() {
+          setCurrentPageUrl(nextPageUrl);
+     }
+
+     function gotoPrevPage() {
+          setCurrentPageUrl(prevPageUrl);
+     }
+
+     return (
+          <div>
+               {pokemon.map((pokemon, index) => (
+                    <div key={pokemon.name}>{pokemon.name}</div>
+               ))}
+               <div>
+                    {prevPageUrl && <button onClick={gotoPrevPage}>Previous</button>}
+                    {nextPageUrl && <button onClick={gotoNextPage}>Next</button>}
+               </div>
+          </div>
+     )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     return (
+          <div className="App">
+            <h1>Pokedex</h1>
+            <PokemonList />
+          </div>
+     );
 }
 
 export default App;

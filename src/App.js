ddac409437cca0bb1bae10003a668811fd0352e2
Rasmus B.K. Nielsen <rasmus.bk.nielsen@gmail.com>
2024-04-16 08:21:31 +0200

    Moved content to Pages folder

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-function PokemonList() {
-     const [pokemon, setPokemon] = useState([]);
-     const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
-     const [nextPageUrl, setNextPageUrl] = useState();
-     const [prevPageUrl, setPrevPageUrl] = useState();
+import Pokedex from './Pages/Pokedex';
 
-     useEffect(() => {
-          async function fetchData() {
-               const response = await fetch(currentPageUrl);
-               const data = await response.json();
-               setPokemon(data.results);
-               setNextPageUrl(data.next);
-               setPrevPageUrl(data.previous);
-          }
-          fetchData();
-     }, [currentPageUrl]);
-
-     function gotoNextPage() {
-          setCurrentPageUrl(nextPageUrl);
-     }
-
-     function gotoPrevPage() {
-          setCurrentPageUrl(prevPageUrl);
-     }
-
-     return (
-          <div>
-               {pokemon.map((pokemon, index) => (
-                    <div key={pokemon.name}>{pokemon.name}</div>
-               ))}
-               <div>
-                    {prevPageUrl && <button onClick={gotoPrevPage}>Previous</button>}
-                    {nextPageUrl && <button onClick={gotoNextPage}>Next</button>}
-               </div>
-          </div>
-     )
-}
 
 function App() {
      return (
           <div className="App">
             <h1>Pokedex</h1>
-            <PokemonList />
+            <Pokedex />
           </div>
      );
 }
---
+++ src/Pages/Pokedex.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+function Pokedex() {
+     const [pokemon, setPokemon] = useState([]);
+     const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+     const [nextPageUrl, setNextPageUrl] = useState();
+     const [prevPageUrl, setPrevPageUrl] = useState();
+
+     useEffect(() => {
+          async function fetchData() {
+               const response = await fetch(currentPageUrl);
+               const data = await response.json();
+               setPokemon(data.results);
+               setNextPageUrl(data.next);
+               setPrevPageUrl(data.previous);
+          }
+          fetchData();
+     }, [currentPageUrl]);
+
+     function gotoNextPage() {
+          setCurrentPageUrl(nextPageUrl);
+     }
+
+     function gotoPrevPage() {
+          setCurrentPageUrl(prevPageUrl);
+     }
+
+     return (
+          <div>
+               {pokemon.map((pokemon, index) => (
+                    <div key={pokemon.name}>{pokemon.name}</div>
+               ))}
+               <div>
+                    {prevPageUrl && <button onClick={gotoPrevPage}>Previous</button>}
+                    {nextPageUrl && <button onClick={gotoNextPage}>Next</button>}
+               </div>
+          </div>
+     )
+}

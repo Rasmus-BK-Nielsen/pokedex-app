2c5a8c8a7a1f62ac08439a182cff563334709fb2
Rasmus B.K. Nielsen <rasmus.bk.nielsen@gmail.com>
2024-04-18 14:25:39 +0200

    UI elements fexed, navbar placement corrected.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./App.css";
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Pokedex from './Pages/Pokedex';
+import './App.css';
 
 function NavBar() {
-     return (
-          <div>
-               <Link to="/pokedex">Pokedex</Link>
-          </div>
-     )
+    return (
+        <Router>
+            <div className="Navbar">
+                <Link to="/pokedex">Pokedex</Link>
+            </div>
+            <div className="Content">
+                <Routes>
+                    <Route path="/" element={<Pokedex />} />
+                    <Route path="/pokedex" element={<Pokedex />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
 export default NavBar;

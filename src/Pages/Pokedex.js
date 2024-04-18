cf97b27995222068d0885a8a92e202d3fbbc56e9
Rasmus B.K. Nielsen <rasmus.bk.nielsen@gmail.com>
2024-04-18 14:44:48 +0200

    Backgroundcolours and text fixed

--- src/Pages/Pokedex.js
+++ src/Pages/Pokedex.js
@@ -1,6 +1,27 @@
 import '../App.css';
 import React, { useState, useEffect } from 'react';
 
+const typeColors = {
+     bug: '#A8B820',
+     dark: '#705848',
+     dragon: '#7038F8',
+     electric: '#F8D030',
+     fairy: '#EE99AC',
+     fighting: '#C03028',
+     fire: '#F08030',
+     flying: '#A890F0',
+     ghost: '#705898',
+     grass: '#78C850',
+     ground: '#E0C068',
+     ice: '#98D8D8',
+     normal: '#A8A878',
+     poison: '#A040A0',
+     psychic: '#F85888',
+     rock: '#B8A038',
+     steel: '#B8B8D0',
+     water: '#6890F0'
+};
+
 function Pokedex() {
      const [pokemon, setPokemon] = useState([]);
      const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon');
@@ -38,7 +59,7 @@ function Pokedex() {
           <div className="Container">
                <div className="Pokedex">
                     {pokemon.map((pokemon, index) => (
-                         <div key={pokemon.name} className="Pokemon-Card">
+                         <div key={pokemon.name} className="Pokemon-Card" style={{backgroundColor: typeColors[pokemon.types[0].type.name]}}>
                               <div className="Text">
                                    <p># {pokemon.id}</p>
                                    <h2>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
